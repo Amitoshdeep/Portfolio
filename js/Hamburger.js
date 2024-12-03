249b70a9d907de8af66d1306c3d82ea6a43d3868
Amitoshdeep <104307738+Amitoshdeep@users.noreply.github.com>
2024-12-03 19:44:59 +0530

    Delete js directory

--- js/Hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.querySelector(".hamburger");
-    const navbar = document.querySelector(".navbar");
-
-    hamburger.addEventListener("click", () => {
-        navbar.classList.toggle("active"); // Toggle the active class
-    });
-});
