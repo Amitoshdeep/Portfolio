885f822e991b80a27f326eaa75c549c312ed81d8
Amitoshdeep Singh <amitosh1999@gmail.com>
2025-05-11 19:55:48 +0530

    close js

--- assets/js/hamBg.js
+++ assets/js/hamBg.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#navB').addEventListener('click', () => {
     const side = document.querySelector('.sidebar');
-    const navImg = document.querySelector('#navB'); // Assuming there's an <img> inside #navB
+    const navImg = document.querySelector('#navB img'); // Assuming there's an <img> inside #navB
 
     if (side.classList.contains('hidden')) {
     //   console.log('Sidebar is hidden');
       side.classList.remove('hidden');
 
       // Change nav image to "close" icon
-      navImg.src = 'assets/icons/close.svg';
+      navImg.src = '/assets/icons/close.svg';
     } else {
     //   console.log('Sidebar is visible');
       side.classList.add('hidden');
